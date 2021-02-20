eb01e89ac19adb59d910dc27d2ac8d50aa74cd3c
dwayneglevene <dwayneglevene@yahoo.com>
2021-02-20 23:13:46 +0000

    Updates

---
+++ precache-manifest.928149ef153da9dcbfaec8073d037447.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004fa63ac34e5321ab7c31742f2b6aee",
+    "url": "/TodoappCodebucks/index.html"
+  },
+  {
+    "revision": "e01ed66056759c202e07",
+    "url": "/TodoappCodebucks/static/css/main.00abd70f.chunk.css"
+  },
+  {
+    "revision": "8918d0af2062251e56c3",
+    "url": "/TodoappCodebucks/static/js/2.fb5ada59.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/TodoappCodebucks/static/js/2.fb5ada59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e01ed66056759c202e07",
+    "url": "/TodoappCodebucks/static/js/main.2687c68f.chunk.js"
+  },
+  {
+    "revision": "9e1ca2c7a73b192f0ce1",
+    "url": "/TodoappCodebucks/static/js/runtime-main.c0690ba3.js"
+  },
+  {
+    "revision": "4f646371b2a2501c7acd9110625993ca",
+    "url": "/TodoappCodebucks/static/media/background.4f646371.jpg"
+  }
+]);
